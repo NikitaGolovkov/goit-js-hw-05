607764055b9892a01e19b5e870b86d72eaf2ff63
Nikita Golovkov <nikitagolovkov@gmail.com>
2021-01-19 14:15:41 +0200

    Update ( _price)

--- js/task-5.js
+++ js/task-5.js
@@ -7,7 +7,7 @@ class Car {
 
   static getSpecs(car) {
     console.log(
-      `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`
+      `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car._price}`
     );
   }
 
